99d424a73cabde4cf79169af01cb5953d5374b3c
Ramraghul <raghulraghul111@gmail.com>
2022-12-26 19:38:02 +0530

    API changes

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,5 +1,5 @@
 
-export const host = "http://localhost:5000";
+export const host = "https://pizza-app-backend-drab.vercel.app";
 
 export const getPizzaData = `${host}/api/pizzas/getAllPizzas`;
 export const addNewPizza = `${host}/api/pizzas/addpizza`;
